0a4b83f001addcc1e6bfd3902ae04c2e8aebe744
Ben Fiola <benfiola@gmail.com>
2023-02-25 16:02:32 -0800

    Implement click and fastapi extension scaffold

---
+++ asyncio_task_queues/click.py
@@ -0,0 +1,19 @@
+import click
+import click.core
+
+from asyncio_task_queues.app import App
+
+
+class Group(click.core.Group):
+    app: App
+
+    def __init__(self, app: App):
+        super().__init__()
+        self.app = app
+
+        commands = [("ping", self.ping)]
+        for name, callback in commands:
+            self.add_command(click.Command(name=name, callback=callback))
+
+    def ping(self):
+        click.echo("pong")
--- asyncio_task_queues/fastapi.py
+++ asyncio_task_queues/fastapi.py
@@ -1,7 +1,19 @@
 import fastapi
 import fastapi.routing
 
+from asyncio_task_queues.app import App
+
 
 class APIRouter(fastapi.routing.APIRouter):
+    app: App
+
     def __init__(self, app: App):
-        pass
+        super().__init__()
+        self.app = app
+
+        routes = [("/ping", self.ping, ["GET"])]
+        for path, endpoint, methods in routes:
+            self.add_api_route(path=path, endpoint=endpoint, methods=methods)
+
+    async def ping(self) -> str:
+        return "pong"
